d4f9dd1216ada95d7295c41ceb346e5805189d03
Cesare Camurani <cesarecamurani@Cesares-MacBook-Pro.local>
2024-11-07 14:19:06 +0100

    Minor refactoring in TaxesCalculator namings

--- lib/taxes_calculator.rb
+++ lib/taxes_calculator.rb
@@ -2,29 +2,29 @@ class TaxesCalculator
   BASIC_SALES_TAX_RATE = 0.10.freeze
   IMPORT_DUTY_TAX_RATE = 0.05.freeze
 
-  IMPORTED = "imported".freeze
+  IMPORTED_KEY = "imported".freeze
   EXEMPT_ITEMS = %w[book chocolate pill].freeze
 
   class << self
     def calculate_taxes(item_name, price)
       tax_rate = 0
 
-      tax_rate += IMPORT_DUTY_TAX_RATE if imported_good?(item_name)
+      tax_rate += IMPORT_DUTY_TAX_RATE if imported_item?(item_name)
 
-      tax_rate += BASIC_SALES_TAX_RATE unless exempted_good?(item_name)
+      tax_rate += BASIC_SALES_TAX_RATE unless exempt_item?(item_name)
 
       round_to_nearest_0_05(price * tax_rate)
     end
 
     private
 
-    def imported_good?(item_name)
-      item_name.downcase.include?(IMPORTED)
+    def imported_item?(item_name)
+      item_name.downcase.include?(IMPORTED_KEY)
     end
 
-    def exempted_good?(item_name)
-      EXEMPT_ITEMS.any? do |exempted_item|
-        item_name.downcase.match?(/\b#{Regexp.escape(exempted_item)}s?\b/)
+    def exempt_item?(item_name)
+      EXEMPT_ITEMS.any? do |exempt_item|
+        item_name.downcase.match?(/\b#{Regexp.escape(exempt_item)}s?\b/)
       end
     end
 
